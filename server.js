dbeeac2d182fb45e8f81a9e51f4e72fa17662af6
leejoe03 <joelee2003@gmail.com>
2024-02-16 15:12:14 +1000

    created .env

--- server.js
+++ server.js
@@ -5,4 +5,7 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
-app.use("/api/v1/reviews")
+app.use("/api/v1/reviews", reviews)
+app.use("*", (req, res) => res.status(404).jason({error: "not found"}))
+
+export default app
